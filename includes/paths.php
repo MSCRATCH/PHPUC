b5a6c0e670420d15ebe606f9cf16be155ea74416
MSCRATCH <me@pathologicalplay.com>
2026-01-22 19:32:25 +0100

    Add files via upload

---
+++ includes/paths.php
@@ -0,0 +1,22 @@
+<?php
+defined('MAIN') or die("Direct access to this file is restricted.");
+
+//This file is part of PHPUC
+//paths.php
+//MMXXVI MSCRATCH
+
+define('BASE_URL', '/');
+
+define('root_path', __DIR__ . '/..');
+define('config_path', root_path . '/config');
+define('frontend_functions_path', root_path . '/functions/frontend_functions');
+define('backend_functions_path', root_path . '/functions/backend_functions');
+define('shared_functions_path', root_path . '/functions/shared_functions');
+define('includes_path', root_path . '/includes');
+define('templates_path', root_path . '/templates');
+define('handlers_path', root_path . '/handlers');
+define('themes_path', root_path . '/themes');
+define('logs_path', root_path . '/logs');
+define('contents_path', root_path . '/contents');
+define('profile_images_path', root_path . '/profile_images');
+define('files_path', root_path . '/files');
